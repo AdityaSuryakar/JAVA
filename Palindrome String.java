b43cc84cae4a79c28a21cbab84e888f97ed297ad
Aditya Suryakar <suryakaraditya@gmail.com>
2025-07-07 11:44:54 +0530

    Create Palindrome String.java

---
+++ Palindrome String.java
@@ -0,0 +1,24 @@
+//Q3. Write a java code to check string is palindrome. 
+
+package com.dkte;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        String str = "pop";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
