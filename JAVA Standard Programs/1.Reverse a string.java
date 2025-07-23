ad54bcd2d10fcc735894876b2833434725cd14b3
Aditya Suryakar <suryakaraditya@gmail.com>
2025-07-23 22:09:22 +0530

    Create 1.Reverse a string.java

---
+++ JAVA Standard Programs/1.Reverse a string.java
@@ -0,0 +1,16 @@
+//1.Reverse a string
+
+package com.interview;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "SURYAKAR";
+		System.out.println(str);
+		StringBuilder reversed = new StringBuilder(str).reverse();
+		System.out.println(reversed);
+
+	}
+
+}
