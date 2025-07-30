00b55f2d6a444ed4eaf1d8cc3ad9ac752bcfc864
Aditya Suryakar <suryakaraditya@gmail.com>
2025-07-30 23:10:05 +0530

    Create 12.Count the Number of Digits in a Number.java

---
+++ JAVA Standard Programs/12.Count the Number of Digits in a Number.java
@@ -0,0 +1,14 @@
+//12.Count the Number of Digits in a Number
+package com.dkte;
+
+public class CountDigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 12345;
+		int count = 0;
+		count = String.valueOf(num).length();
+		System.out.println("Count :"+count);
+	}
+
+}
